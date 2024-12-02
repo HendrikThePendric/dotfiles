9416f90a82e10733c356b58ae4f845017fbb9b68
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2024-12-02 21:00:22 +0100

    highlight all treesitter scopes (and see how that goes)

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,14 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {
+    scope = {
+      enabled = true,
+      include = {
+        node_type = { ["*"] = { "*" } },
+      },
+    },
+  },
+}
