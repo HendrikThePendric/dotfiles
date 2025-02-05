d311c9b850f80d24c4fdc72a58182eb085fcecdd
HendrikThePendric <hendrik@dhis2.org>
2025-02-05 11:45:55 +0100

    chore: clear lsp config so it can be resynced correctly

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,18 +1,1 @@
-return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
-    diagnostics = {
-      virtual_text = false,
-    },
-    servers = {
-      vtsls = {
-        settings = {
-          vtsls = {
-            autoUseWorkspaceTsdk = false,
-          },
-        },
-      },
-    },
-  },
-}
+return {}
