51e498aa67ace27bfc2480874de61e0a38244870
HendrikThePendric <hendrik@dhis2.org>
2025-02-05 11:47:12 +0100

    chore: disable inline TS hints and ESLint errors and use globally installed TS version by default

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,1 +1,18 @@
-return {}
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    inlay_hints = { enabled = false },
+    diagnostics = {
+      virtual_text = false,
+    },
+    servers = {
+      vtsls = {
+        settings = {
+          vtsls = {
+            autoUseWorkspaceTsdk = false,
+          },
+        },
+      },
+    },
+  },
+}
