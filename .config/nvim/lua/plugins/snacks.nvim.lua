cb53eebd49a0f8aaa3634b2a031deb497a968a5c
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2025-06-09 17:31:31 +0200

    chore: swap neo-tree for snacks explorer and add some sensible keybindings

--- .config/nvim/lua/plugins/snacks.nvim.lua
+++ .config/nvim/lua/plugins/snacks.nvim.lua
@@ -12,5 +12,39 @@ return {
     lazygit = {
       enabled = false,
     },
+    explorer = { enabled = true }, -- NOT HERE!
+    picker = {
+      enabled = true,
+      sources = {
+        explorer = { -- HERE!
+          enabled = true,
+          hidden = true,
+          auto_close = false,
+          win = {
+            list = {
+              keys = {
+                ["O"] = { { "pick_win", "jump" }, mode = { "n", "i" } },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        local explorer_pickers = Snacks.picker.get({ source = "explorer" })
+        if #explorer_pickers == 0 then
+          Snacks.picker.explorer()
+        elseif explorer_pickers[1]:is_focused() then
+          explorer_pickers[1]:close()
+        else
+          explorer_pickers[1]:focus()
+        end
+      end,
+      desc = "File explorer",
+    },
   },
 }
