0afc7ba3e91050333110a9bad53e0293944ed8a4
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2026-01-10 14:39:26 +0100

    chore: improve copy-paste behaviour

--- shared/.config/nvim/lua/config/options.lua
+++ shared/.config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- ============================================================================
+-- Clipboard
+-- ============================================================================
+-- Sync with system clipboard (uses wl-copy on Wayland, xclip/xsel on X11)
+vim.opt.clipboard = "unnamedplus"
